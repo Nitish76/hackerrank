11341d959514862f826d40e3262448470e84305a
Nitish76 <36237103+Nitish76@users.noreply.github.com>
2018-09-03 12:30:06 -0500

    Comments added

--- Diagonal difference.py
+++ Diagonal difference.py
@@ -6,15 +6,16 @@ import random
 import re
 import sys
 
-# Complete the diagonalDifference function below.
+# Function to calculate the absolute difference between the sums of its diagonals.
 def diagonalDifference(a):
     sum1 = 0; sum2 = 0
     #n = max(len(a))
     for i in range(n):
-        sum1 += a[i][i]
-        sum2 += a[i][n-i-1]
+        sum1 += a[i][i]             # sum of left to right diagonal
+        sum2 += a[i][n-i-1]         # sum of right to left diagonal
     return(abs(sum1-sum2))
 
+#Input and output commands
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
