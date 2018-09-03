90ed193a45532bbd3a4ed88a13d3b22ea5827fce
Nitish76 <36237103+Nitish76@users.noreply.github.com>
2018-09-03 12:33:04 -0500

    Comments added

--- Plus minus.py
+++ Plus minus.py
@@ -6,9 +6,10 @@ import random
 import re
 import sys
 
-# Complete the plusMinus function below.
+# Calculate the fractions of an arry's elements that are positive, negative, and are zeros
 def plusMinus(arr):
     neg = 0; pos = 0; zer = 0
+    # loop to calculate number of positive, negative and zero elements
     for i in arr:
         if i==0:
             zer += 1
@@ -16,12 +17,15 @@ def plusMinus(arr):
             pos += 1
         else:
             neg +=1
-    neg =neg/float(n); pos = pos/float(n); zer = zer/float(n)
+    neg =neg/float(n); pos = pos/float(n); zer = zer/float(n)       # fraction calculation
+    
+    # Output commands
     print(pos)
     print(neg)
     print(zer)   
     return()
 
+# Input commands
 if __name__ == '__main__':
     n = int(input())
 
