7d50dcac17402b0a2148c4124c9c81d1f58dccdd
Nitish76 <36237103+Nitish76@users.noreply.github.com>
2018-09-02 19:15:06 -0500

    Update Compare the triplets.py

--- Compare the triplets.py
+++ Compare the triplets.py
@@ -6,7 +6,9 @@ import random
 import re
 import sys
 
-# Complete the solve function below.
+# A function that compares Alice and Bob's rating and assigns them points accordingly.
+# +1 point to the person with higher rating
+# no points to both people if their ratings are the same
 def solve(a, b):
     A=0; B=0
     for i in range(3):
@@ -19,12 +21,13 @@ def solve(a, b):
             B=B
     return(A,B)
 
+#input and output statements
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    a = list(map(int, input().rstrip().split()))
+    a = list(map(int, input().rstrip().split()))  #Alice's ratings
 
-    b = list(map(int, input().rstrip().split()))
+    b = list(map(int, input().rstrip().split()))  # Bob's ratings
 
     result = solve(a, b)
 
