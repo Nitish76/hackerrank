c38905cfcbb20c7e576b4add4fa4c5df735a3d23
Nitish Goyal <36237103+Nitish76@users.noreply.github.com>
2018-09-04 18:25:11 -0500

    Comments added

--- Time conversion.py
+++ Time conversion.py
@@ -3,27 +3,26 @@
 import os
 import sys
 
-#
-# Complete the timeConversion function below.
-#
+
+# A function to convert 12-hour AM/PM time to millitary time
+
 def timeConversion(s):
-    hr = (s[0:2]); min = s[3:5]; sec = s[6:8];ap = s[8:9]
-    if ap == 'A':
+    hr = (s[0:2]); min = s[3:5]; sec = s[6:8];ap = s[8:9]       # Extracting hours, minutes, seconds and AM/PM to different variables
+    if ap == 'A':                                               # Conversion for AM case
         if int(hr)== 12:
             hr = str('00')
             time = hr +':'+min+':'+sec
         else:
             time = hr +':'+min+':'+sec
-    else:
+    else:                                                       # Conversion for PM case
         if int(hr) == 12:
             time = hr +':'+min+':'+sec
         else:
             hr = str(int(hr)+12)
             time = hr+':'+min+':'+sec
     return(time)
-    # Write your code here.
-    #
-
+    
+# Input statements
 if __name__ == '__main__':
     f = open(os.environ['OUTPUT_PATH'], 'w')
 
