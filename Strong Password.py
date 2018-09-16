1e8eb3977a83dd7214676fbce31b5b5271506197
Nitish Goyal <36237103+Nitish76@users.noreply.github.com>
2018-09-15 20:00:28 -0500

    Added problem description, input constraint and comments

--- Strong Password.py
+++ Strong Password.py
@@ -6,12 +6,22 @@ import random
 import re
 import sys
 
-# Complete the minimumNumber function below.
+'''The website considers a password to be strong if it satisfies the following criteria:
+    Its length is at least 6
+    It contains at least one digit
+    It contains at least one lowercase English character
+    It contains at least one uppercase English character
+    It contains at least one special character. The special characters are: !@#$%^&*()-+ '''
+
+# The functionbelow checks if the given password is strong
+# If not, then it returns the minimum number of characters to be added to make it strong
+# 1<= n <=100   (n is length of input password)
+
 def minimumNumber(n, password):
-    num = "0123456789"; is_num = 0
-    lc = "abcdefghijklmnopqrstuvwxyz"; is_lc = 0
-    uc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; is_uc = 0
-    sc = "!@#$%^&*()-+"; is_sc = 0
+    num = "0123456789"; is_num = 0                  #digits and their counter
+    lc = "abcdefghijklmnopqrstuvwxyz"; is_lc = 0    #lowercase letters and their counter
+    uc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; is_uc = 0    #uppercase letters and their counter
+    sc = "!@#$%^&*()-+"; is_sc = 0                  #special characters and their counter
     for i in password:
         if i in num:
             is_num +=1
@@ -21,7 +31,7 @@ def minimumNumber(n, password):
             is_uc += 1
         if i in sc:
             is_sc += 1
-    total = 0;
+    total = 0;                                      # variable to count min no. of characters needed 
     if is_num>0:
         pass
     else:
@@ -44,6 +54,7 @@ def minimumNumber(n, password):
         total += (6-n-total) 
     return(total)
 
+#input and output statements
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
