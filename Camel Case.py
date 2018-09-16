a9c3fcb87ba401f7739291fd111afe47a6d2f4b3
Nitish Goyal <36237103+Nitish76@users.noreply.github.com>
2018-09-15 19:52:01 -0500

    Added problem statement and comments

--- Camel Case.py
+++ Camel Case.py
@@ -6,7 +6,12 @@ import random
 import re
 import sys
 
-# Complete the camelcase function below.
+''' Camel case is the concatenation of one or more english words such that
+    the first word is all lower case and the consequent words have their
+    first letter as upper case and rest as lower case'''
+# The function below counts the number of words given a string(s) in Camel case
+# 1<= s <= 10.0E+5
+
 def camelcase(s):
     count = 0
     for i in s:
@@ -15,7 +20,7 @@ def camelcase(s):
     return(count+1)
         
             
-
+# Input and output statements
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
